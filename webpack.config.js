1237070e056d43706cb87c3ab43e6f39676f35d7
Adrian Pearman <adrianpearman12@gmail.com>
2019-03-25 15:12:39 -0400

    adding in sass and css style loading. updated index.html with removing the script file

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,17 @@ module.exports = {
     "output": {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    module:{
+        rules:[
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', // inject content to DOM
+                    'css-loader', //compiles CSS to JavScript
+                    'sass-loader' //converts SASS into CSS
+                ]
+            }
+        ]
     }
 }
